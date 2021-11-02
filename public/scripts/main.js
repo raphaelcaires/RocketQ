2c3c5115be7d22d77aeddf2e82b9be3d59f051a1
Raphael Caires <75398550+raphaelcaires@users.noreply.github.com>
2021-11-02 13:58:33 -0300

    script refatorado

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,18 +2,31 @@ import Modal from './modal.js';
 
 const modal = Modal();
 
+const modalTitle = document.querySelector('.modal h2');
+const modalDescription = document.querySelector('.modal p');
+const modalButton = document.querySelector('.modal button');
+
+
 const checkButtons = document.querySelectorAll(".actions a.check");
 
 checkButtons.forEach(button => {
-  button.addEventListener("click", event => {
-    modal.open();
-  });
+  button.addEventListener("click", handleClick);
 });
 
 const deleteButtons = document.querySelectorAll(".actions a.delete");
 
 deleteButtons.forEach(button => {
-  button.addEventListener("click", event => {
-    modal.open();
-  });
+  button.addEventListener("click", (event) => handleClick(event, false));
 });
+
+function handleClick(event, check = true){
+  event.preventDefault();
+  const text = check ? "Marcar como lida" : "Excluir";
+
+  modalTitle.innerHTML = `${text}`;
+  modalDescription.innerHTML = `Tem certeza que deseja ${text.toLowerCase()} esta pergunta?`;
+  modalButton.innerHTML = `${text.toLowerCase()}`;
+  check ? modalButton.classList.remove("red") : modalButton.classList.add("red");
+  
+  modal.open();
+};
