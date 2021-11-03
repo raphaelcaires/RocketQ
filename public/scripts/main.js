4f4dee3067847e7055db7a6cda1686ffcc9a792e
Raphael Caires <75398550+raphaelcaires@users.noreply.github.com>
2021-11-02 21:16:45 -0300

    correção de bug no modal

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -22,6 +22,12 @@ deleteButtons.forEach(button => {
 function handleClick(event, check = true){
   event.preventDefault();
   const text = check ? "Marcar como lida" : "Excluir";
+  const slug = check ? "check" : "delete";
+  const roomId = document.querySelector("#room-id").dataset.id;
+  const questionId = event.target.dataset.id;
+
+  const form = document.querySelector(".modal form");
+  form.setAttribute("action", `/room/${roomId}/${questionId}/${slug}`);
 
   modalTitle.innerHTML = `${text}`;
   modalDescription.innerHTML = `Tem certeza que deseja ${text.toLowerCase()} esta pergunta?`;
--- src/route.js
+++ src/route.js
@@ -5,5 +5,6 @@ const route =express.Router();
 route.get('/', (req, res) => res.render("index"));
 route.get('/room', (req, res) => res.render("room"));
 route.get('/create-pass', (req, res) => res.render("create-pass"));
+/* route.get('/room/:room/:question/:action', (req,res) */
 
 module.exports = route;
