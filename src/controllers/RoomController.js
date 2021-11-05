8af2702954c41383cadb1bcadead0686ce434ef2
Raphael Caires <75398550+raphaelcaires@users.noreply.github.com>
2021-11-04 21:02:35 -0300

    ID da sala dinâmico

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -6,21 +6,36 @@ module.exports = {
     const pass = req.body.password
     
     let roomId = "";
+    let isRoom = true;
 
-    for(var i = 0; i < 6; i++){
-      roomId += Math.floor(Math.random() * 10).toString();
-    }
+    while(isRoom){
+      /* Gera o número da sala */
+      for(var i = 0; i < 6; i++){
+        roomId += Math.floor(Math.random() * 10).toString();
+      };
+      /* Verifica se o número da sala já existe */
+      const roomSameIds = await db.all(`SELECT id FROM rooms`);
+      isRoom = roomSameIds.some(roomSameId => roomSameId === roomId);
 
-    await db.run(`INSERT INTO rooms (
-      id,
-      pass
-    ) VALUES (
-      ${parseInt(roomId)},
-      ${pass}
-    )`);
+      if(! isRoom){
+        /* Insere a sala criada no banco de dados */
+        await db.run(`INSERT INTO rooms (
+          id,
+          pass
+        ) VALUES (
+          ${parseInt(roomId)},
+          ${pass}
+        )`);
+      };
+    };
 
     await db.close();
 
     res.redirect(`/room/${roomId}`);
+  },
+
+  open(req, res){
+    const roomId = req.params.room;
+    res.render("room", {roomId: roomId});
   }
 };
