e5d6c0ddb5562eabbf73dd5b16cc2521e6a8c335
Raphael Caires <75398550+raphaelcaires@users.noreply.github.com>
2021-11-05 21:55:59 -0300

    iserir perguntas do banco de dados

--- src/route.js
+++ src/route.js
@@ -1,3 +1,4 @@
+const { Router } = require('express');
 const express = require('express');
 const QuestionController = require('./controllers/QuestionController');
 const RoomController = require('./controllers/RoomController');
@@ -7,9 +8,10 @@ const route =express.Router();
 route.get('/', (req, res) => res.render("index", {page: 'enter-room'}));
 route.get('/create-pass', (req, res) => res.render("index", {page: 'create-pass'}));
 
+route.post('/create-room', RoomController.create);
 route.get('/room/:room', RoomController.open);
 
+route.post ('/question/create/:room', QuestionController.create);
 route.post('/question/:room/:question/:action', QuestionController.index);
-route.post('/create-room', RoomController.create);
 
 module.exports = route;
